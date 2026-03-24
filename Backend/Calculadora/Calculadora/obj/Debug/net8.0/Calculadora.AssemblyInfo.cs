77ae49cbcc168b4fb181cf86c006e76660f485ba
jonathan-0703 <leoalava2016@gmail.com>
2026-03-24 12:27:41 -0500

    Actualiación de .NET

--- Backend/Calculadora/Calculadora/obj/Debug/net8.0/Calculadora.AssemblyInfo.cs
+++ Backend/Calculadora/Calculadora/obj/Debug/net8.0/Calculadora.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ef34a5df0c6078882ed8376f40edf182bf5a943")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d7a9f719ff818d01855d775ed67cbbcb3bc4891")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
